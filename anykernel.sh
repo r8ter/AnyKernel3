fe89835d0d09101cee72fc7ef4446c385e649d3f
zhlhlf <60917750+zhlhlf@users.noreply.github.com>
2024-03-27 15:34:25 +0800

    Add files via upload

--- anykernel.sh
+++ anykernel.sh
@@ -1,40 +1,33 @@
-# AnyKernel3 Ramdisk Mod Script
-# osm0sis @ xda-developers
+### AnyKernel3 Ramdisk Mod Script
+## osm0sis @ xda-developers
 
-## AnyKernel setup
+### AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=kernel-by_zhlhlf
-do.devicecheck=1
+kernel.string=by_zhlhlf
+do.devicecheck=0
 do.modules=0
-do.systemless=1
+do.systemless=0
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=instantnoodle
-device.name2=instantnoodlep
-device.name3=lemonkebab
-device.name4=kebab
-device.name5=OnePlus8
-device.name6=OnePlus8Pro
-device.name7=OnePlus8T
-device.name8=lemonades
-device.name9=OnePlus9R
 supported.versions=
 supported.patchlevels=
 '; } # end properties
 
 # shell variables
 block=boot;
-is_slot_device=1;
+is_slot_device=auto;
 ramdisk_compression=auto;
-
+patch_vbmeta_flag=auto;
 
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
 . tools/ak3-core.sh;
 
-## AnyKernel install
+## AnyKernel boot install
 dump_boot;
 
+vbmeta_disable_verification;
+
 write_boot;
-## end install
+## end boot install
